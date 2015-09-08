f37ccd848dbb0b82ec1db7a230d612f6d8a0e971
ori livni <ori@orisomething.com>
2015-09-08 10:49:53 +0300

    Revert "default environment for new API"
    This reverts commit 10a05edf2750d2abea5f10b68c575d234d24dff9.

--- config/environment.js
+++ config/environment.js
@@ -1,28 +1,5 @@
 'use strict';
 
 module.exports = function(/* environment, appConfig */) {
-  return {
-    APP: {
-      CLOUDINARY: {
-        /** For future support */
-        API_KEY: '',
-        /** The user in Cloudinary */
-        CLOUD_NAME: '',
-        /** Used for private CDN or as for sub-domain for given domain */
-        SUB_DOMAIN: '',
-        /** The domain of the account if exists (default to shared domain of Cloudinary) */
-        DOMAIN: '',
-        /** Use HTTPs or HTTP. The default is HTTPs */
-        SECURE: true,
-        /** Use distributions CDN (example: https://res-[1-5].cloudinary.com) */
-        CDN_DISTRIBUTION: false,
-        /** Transforms for concatenation with given transforms */
-        CONCATENATED_TRANSFORMS: [],
-        /** Default transforms that can be override */
-        DEFAULT_TRANSFORMS: [],
-        /** Default images file extensions */
-        DEFAULT_IMAGE_FORMAT: 'jpg'
-      }
-    }
-  };
+  return { };
 };
