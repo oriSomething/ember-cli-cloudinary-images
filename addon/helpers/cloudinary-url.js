ad2ec0b96c56a5d8b93811f3b808fe3ad6236207
brou <brousse@openmailbox.org>
2016-07-15 12:29:15 +0200

    public computeUrl method in the cloudinary service
    This commit proposes a solution to security leaks caused by
    some attribute bindings in ember 2.0+.
    
    Use case
    ========
    
    Applying a cloudinary *background*-image to an element.
    
    Before
    ------
    
    Template:
    ```
    <div style="background-image: url('{{cloudinary-url ...}}');">
    </div>
    ```
    Generates a [security warning](http://emberjs.com/deprecations/v1.x/#toc_binding-style-attributes) from Ember 1.13.
    
    After
    -----
    
    Controller:
    ```
    import Ember from 'ember' ;
    const { inject, computed, String } = Ember ;
    
    export default Ember.Controller.extend({
      cloudinary: inject.service(),
      backgroundCSS: computed( function () {
        const cloudinary = this.get('cloudinary') ;
        var css = "background-image: url('" + cloudinary.cloudinaryUrl(...) + "');" ;
        return String.htmlSafe(css) ;
      })
    }) ;
    ```
    
    Template:
    ```
    <div style={{backgroundCSS}}>
    </div>
    ```
    Is safe and Ember 2.0 compatible.
    
    NOTE:
    The `{{cloudinary-url ...}}` helper is still available to use with classic
    `<img>` elements in templates.

--- addon/helpers/cloudinary-url.js
+++ addon/helpers/cloudinary-url.js
@@ -5,17 +5,6 @@ const { computed, inject, typeOf } = Ember;
 export default Ember.Helper.extend({
   cloudinary: inject.service(),
 
-  /** @property {String[]} concatenatedTransforms */
-  concatenatedTransforms: computed('cloudinary.config.CONCATENATED_TRANSFORMS', function() {
-    // Protection from Strings
-    const concatenatedTransforms = this.get('cloudinary.config.CONCATENATED_TRANSFORMS') || [];
-    return Array.isArray(concatenatedTransforms) ? concatenatedTransforms : [concatenatedTransforms];
-  }),
-  /** @property {String[]} defaultTransforms */
-  defaultTransforms: computed.readOnly('cloudinary.config.DEFAULT_TRANSFORMS'),
-  /** @property {String} defaultImageFormat */
-  defaultImageFormat: computed.readOnly('cloudinary.config.DEFAULT_IMAGE_FORMAT'),
-
   /**
    * @method compute
    * @param  {String}          [publicId]           Public id of image in Cloudinary
@@ -32,69 +21,8 @@ export default Ember.Helper.extend({
    * @param  {String[]}        hash.transforms
    * @return {String}                               URL for image
    */
-  compute([publicId] = [], hash = {}) {
-    /** @validation */
-    if (!publicId) {
-      return '';
-    }
-
-    let {
-      cloudName,
-      width,
-      height,
-      version,
-      domain,
-      subDomain,
-      cdnDistribution,
-      secure,
-      resourceType,
-      type,
-      format,
-      transforms
-    } = hash;
-    // Default params
-    if (typeOf(resourceType) === 'undefined') { resourceType = 'image'; }
-    if (typeOf(type) === 'undefined') { type = 'upload'; }
-    if (typeOf(format) === 'undefined') { format = this.get('defaultImageFormat'); }
-    if (typeOf(transforms) === 'undefined') { transforms = this.get('defaultTransforms'); }
-
-
-    /** @type {String[]} Transforms to concatenate */
-    const concatenatedTransforms = this.get('concatenatedTransforms');
-    /** Convert transforms to array if the given param was string **/
-    transforms = Array.isArray(transforms) ? transforms : transforms || [];
-    /** Adds concatenated transforms **/
-    transforms = concatenatedTransforms.concat(transforms);
-    /** Adds width if exists **/
-    transforms = width || width === 0 ? [`w_${width}`].concat(transforms) : transforms;
-    /** Adds height if exists **/
-    transforms = height || height === 0 ? [`h_${height}`].concat(transforms) : transforms;
-
-    /** Encoding */
-    publicId = encodeURIComponent(decodeURIComponent(publicId)).replace(/%3A/g, ":").replace(/%2F/g, "/");
-    /** Add format if exists */
-    publicId = format ? `${publicId}.${format}` : publicId;
-
-    /** @type {Ember.Service} */
-    const cloudinary = this.get('cloudinary');
-    /** @type {String} */
-    const urlPrefix = cloudinary.publicIdURLPrefix(publicId, {
-      cloudName,
-      subDomain,
-      domain,
-      cdnDistribution,
-      secure
-    });
-
-    /** @type {String} Transforms in String form */
-    const sTransforms = transforms.join(',');
-
-    return (
-      urlPrefix + '/' +
-      `${resourceType}/${type}/` +
-      (sTransforms ? sTransforms + '/' : '') +
-      (version ? `v${version}/` : '') +
-      publicId
-    );
+  compute() {
+    console.log(JSON.stringify(arguments, null, 2)) ;
+    return this.get('cloudinary').cloudinaryUrl(arguments) ;
   }
 });
--- addon/services/cloudinary.js
+++ addon/services/cloudinary.js
@@ -181,5 +181,97 @@ export default Ember.Service.extend({
     }
 
     return `${protocol}${subDomain}.${domain}${pathname}`;
+  },
+
+
+  /** @property {String[]} concatenatedTransforms */
+  concatenatedTransforms: computed('config.CONCATENATED_TRANSFORMS', function() {
+    // Protection from Strings
+    const concatenatedTransforms = this.get('config.CONCATENATED_TRANSFORMS') || [];
+    return Array.isArray(concatenatedTransforms) ? concatenatedTransforms : [concatenatedTransforms];
+  }),
+  /** @property {String[]} defaultTransforms */
+  defaultTransforms: computed.readOnly('config.DEFAULT_TRANSFORMS'),
+  /** @property {String} defaultImageFormat */
+  defaultImageFormat: computed.readOnly('config.DEFAULT_IMAGE_FORMAT'),
+
+  /**
+   * @method cloudinaryUrl
+   * @param  {String}          [publicId]           Public id of image in Cloudinary
+   * @param  {String}          hash.format          File extension)
+   * @param  {String}          hash.cloudName
+   * @param  {(String|Number)} hash.width
+   * @param  {(String|Number)} hash.height
+   * @param  {(String|Number)} hash.version
+   * @param  {String}          hash.domain
+   * @param  {String}          hash.subDomain
+   * @param  {Boolean}         hash.cdnDistribution
+   * @param  {Boolean}         hash.secure
+   * @param  {String}          hash.type
+   * @param  {String[]}        hash.transforms
+   * @return {String}                               URL for image
+   */
+  cloudinaryUrl([publicId] = [], hash = {}) {
+    /** @validation */
+    if (!publicId) {
+      return '';
+    }
+
+    let {
+      cloudName,
+      width,
+      height,
+      version,
+      domain,
+      subDomain,
+      cdnDistribution,
+      secure,
+      resourceType,
+      type,
+      format,
+      transforms
+    } = hash;
+    // Default params
+    if (typeOf(resourceType) === 'undefined') { resourceType = 'image'; }
+    if (typeOf(type) === 'undefined') { type = 'upload'; }
+    if (typeOf(format) === 'undefined') { format = this.get('defaultImageFormat'); }
+    if (typeOf(transforms) === 'undefined') { transforms = this.get('defaultTransforms'); }
+
+
+    /** @type {String[]} Transforms to concatenate */
+    const concatenatedTransforms = this.get('concatenatedTransforms');
+    /** Convert transforms to array if the given param was string **/
+    transforms = Array.isArray(transforms) ? transforms : transforms || [];
+    /** Adds concatenated transforms **/
+    transforms = concatenatedTransforms.concat(transforms);
+    /** Adds width if exists **/
+    transforms = width || width === 0 ? [`w_${width}`].concat(transforms) : transforms;
+    /** Adds height if exists **/
+    transforms = height || height === 0 ? [`h_${height}`].concat(transforms) : transforms;
+
+    /** Encoding */
+    publicId = encodeURIComponent(decodeURIComponent(publicId)).replace(/%3A/g, ":").replace(/%2F/g, "/");
+    /** Add format if exists */
+    publicId = format ? `${publicId}.${format}` : publicId;
+
+    /** @type {String} */
+    const urlPrefix = this.publicIdURLPrefix(publicId, {
+      cloudName,
+      subDomain,
+      domain,
+      cdnDistribution,
+      secure
+    });
+
+    /** @type {String} Transforms in String form */
+    const sTransforms = transforms.join(',');
+
+    return (
+      urlPrefix + '/' +
+      `${resourceType}/${type}/` +
+      (sTransforms ? sTransforms + '/' : '') +
+      (version ? `v${version}/` : '') +
+      publicId
+    );
   }
 });
