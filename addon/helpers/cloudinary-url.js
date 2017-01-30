b33a0aa721bb06f03b39b13f058f5f010b1fe390
brou <brousse@openmailbox.org>
2017-01-30 16:10:02 +0100

    return empty sting for empty public id

--- addon/helpers/cloudinary-url.js
+++ addon/helpers/cloudinary-url.js
@@ -22,6 +22,6 @@ export default Ember.Helper.extend({
    * @return {String}                               URL for image
    */
   compute(publicId, hash) {
-    return this.get('cloudinary').getURL([publicId], hash) ;
+    return this.get('cloudinary').getURL(publicId ? [publicId] : [], hash) ;
   }
 });
